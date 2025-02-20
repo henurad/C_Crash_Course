1598de0dd9aa3a9d6901f5c67ee1938e642359a4
Amin <aminevaaali@gmail.com>
2025-02-20 23:04:26 +0330

    modifying FileDiscriptors/MycAT

--- FileDiscriptors/MyCat/mycat.c
+++ FileDiscriptors/MyCat/mycat.c
@@ -1,24 +1,29 @@
 #include <stdlib.h> // for exit()
 #include <unistd.h> // for read() and write()
 #include <fcntl.h> // for open()
+#include <stdio.h>
 
 #define BUFSIZE 1024
 
 int main(int argc, char **argv){
 	if(argc != 2){
-		write(2, "mycat filename\n",16);
+		fprintf(stderr, "mycat filename\n");
 		exit(1);
 	}
 	
 	int fd = open(argv[1], O_RDONLY, 0);
 	if(fd == -1){
-		write(2, "No File\n", 9);
+		fprintf(stderr, "No File\n");
 		exit(2);
 	}
 	int n = 0;
 	char buff[BUFSIZE];
 	while((n = read(fd, buff, BUFSIZE)) != 0){
-		write(1, buff, n);
+		if(n != write(1, buff, n)){
+			fprintf(stderr, "error while showing the text\n");
+			close(fd);
+			exit(1);
+		}
 	} 
 	close(fd);
 
